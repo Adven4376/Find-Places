1b10daf02fd484a97203db2e9a4b498d566465d5
Adven4376 <josaditya4376@gmail.com>
2026-03-09 00:47:47 +0530

    Final: added new config file

---
+++ FindPlaces - Backend/FindPlaces/src/main/java/com/CommunityMaps/FindPlaces/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.CommunityMaps.FindPlaces.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://find-places-ui-community-maps.vercel.app",
+                                "https://find-places-ui-community-maps-qj2w5ur7o-adven4376-projects.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
